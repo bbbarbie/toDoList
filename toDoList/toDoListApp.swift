496fa856f2626622a061d4bed66137e5edc39792
bbbarbie <barbiezhuhe1007@gmail.com>
2023-08-01 10:39:21 -0700

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by WANG YINI on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
