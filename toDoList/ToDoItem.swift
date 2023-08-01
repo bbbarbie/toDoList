692730e140e022d10a5e08c4ac5d8a288d58bb4c
bbbarbie <barbiezhuhe1007@gmail.com>
2023-08-01 11:36:11 -0700

    Iteration 0

---
+++ toDoList/ToDoItem.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.swift
+//  toDoList
+//
+//  Created by WANG YINI on 8/1/23.
+//
+
+import Foundation
+
+class ToDoItem{
+    var title: String = ""
+    var isImportant: Bool = false
+}
